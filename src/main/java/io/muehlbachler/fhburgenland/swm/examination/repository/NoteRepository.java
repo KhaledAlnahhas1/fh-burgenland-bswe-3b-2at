8d5e370e268dd2e41cbc7dd58468fc81e69c7e7e
KhaledAlnahhas1 <114687825+KhaledAlnahhas1@users.noreply.github.com>
2024-02-06 15:21:27 +0100

    docs(javadoc): update documentation in classes (#8)
    Updated the comments in the classes(NoteController, NoteRepository, NoteService,NoteServiceImpl, PersonController, PersonRepository, PersonService, PersonServiceImpl) to Improve clarity and precision.

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
@@ -6,6 +6,16 @@ import org.springframework.data.repository.CrudRepository;
 
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
+/**
+ * Manages notes in the database.
+ */
 public interface NoteRepository extends CrudRepository<Note, String> {
+
+    /**
+     * Finds notes that include a specific content.
+     *
+     * @param content The text to look for within notes.
+     * @return List of notes matching the specified content.
+     */
     List<Note> findByContentContaining(String content);
 }
--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteService.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteService.java
@@ -6,9 +6,28 @@ import java.util.Optional;
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
 public interface NoteService {
+
+    /**
+     * Gets a specific note by its unique ID.
+     *
+     * @param id The ID of the note you want to retrieve.
+     * @return The note if found, or an empty result if not.
+     */
     Optional<Note> get(String id);
 
+    /**
+     * Creates a new note.
+     *
+     * @param note The note you want to create.
+     * @return The newly created note.
+     */
     Note create(Note note);
 
+    /**
+     * Searches for notes based on their content.
+     *
+     * @param query The content you're looking for within notes.
+     * @return A list of notes matching the specified content.
+     */
     List<Note> queryByContent(String query);
 }
