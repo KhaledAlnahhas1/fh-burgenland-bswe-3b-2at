ac73da6a9497ee0be0786f7af874c4ad76edfd94
Khaled <2210859032@fh-burgenland.at>
2024-02-10 18:34:54 +0100

    chore: update NoteServiceTest
    - Updated NoteServiceTest & removed unused import

--- src/test/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteServiceTest.java
+++ src/test/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteServiceTest.java
@@ -1,7 +1,6 @@
 package io.muehlbachler.fhburgenland.swm.examination.service;
 
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
-import io.muehlbachler.fhburgenland.swm.examination.service.impl.NoteServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
